62bdb6f8ee03e79a367c69d7325709a4dfb171d8
michele <sload@smartload1>
2020-04-05 16:05:32 +0300

    First version

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
-# oprecorder - Operator Recorder
-The goal of this software package is to record the world state and the actions performed by an operator.
+# realLife - Run everything needed to run the real car
+The goal of this software package is to run anything needed in order to communicate with the real vehicle.
+It is mainly a launch file.
 
-It starts a ROS node called /oprecorder that monitors the following topics:
-* /mavros/local_position/pose (PoseStamped)
-* /mavros/local_position/velocity (TwistStamped)
-* /arm/height (Int32)
-* /arm/blade/Imu (Imu)
-* /mavros/imu/data (Imu)
-* /stone/1/Pose (PoseStamped)
-* /stone/2/Pose (PoseStamped)
-* /joy (Joy)
-
-Each time the callback of the action (joystick) is called, it changes the world state accordingly and saves the:
-Date&Time, world_state, grade (N/A) by adding a row in the csv file: oprecorder.csv
+It starts by trying to run: 
+ * rosserial for connecting to the ARDUINO board.
+ * apm.launch file for connecting to the pixhawk via MAVROS.
+ * the velodyne node and nodelets
+ * the innovation pose node
+ * the quaternion to roll pitch yaw node
+ * the recorder node
+ * the rc node
+ 
+ 
 
 
 
---
+++ src/quat2rpyPose.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+
+
+import rospy
+from std_msgs.msg import Header
+from std_msgs.msg import Int32, Bool
+from std_msgs.msg import String
+from sensor_msgs.msg import Joy
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import PoseStamped, TwistStamped
+import csv
+from datetime import datetime
+
+import tf
+
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import Vector3
+
+loc_RPY_pub = rospy.Publisher('/VehiclePose_RollPithchYaw', Vector3, queue_size=5)
+
+
+def command_calback(msg):
+    quaternion = (
+    msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w)
+    euler = tf.transformations.euler_from_quaternion(quaternion)
+    roll = euler[0]
+    pitch = euler[1]
+    yaw = euler[2]
+
+    RPY = Vector3(roll, pitch, yaw)
+
+    loc_RPY_pub.publish(RPY)
+
+
+rospy.init_node('Pose_RollPithchYaw_pub')
+
+
+def main():
+    rate = rospy.Rate(100)
+    while not rospy.is_shutdown():
+        msg = rospy.wait_for_message("/mavros/local_position/pose", PoseWithCovarianceStamped)
+        command_calback(msg)
+        rate.sleep()
+
+
+main()
